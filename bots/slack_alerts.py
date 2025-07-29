bd0a8b9782951edeadb0df6ed09ef9599c7a79a9
Mike Stucka <stucka@whitedoggies.com>
2025-07-29 12:49:04 -0400

    Linting

--- bots/slack_alerts.py
+++ bots/slack_alerts.py
@@ -1,11 +1,11 @@
-from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
-import os
-from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
 import logging
+import os
+
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
 logger = logging.getLogger(__name__)
+
 
 class SlackInternalAlert:
     def __init__(self, script_name):
@@ -16,13 +16,15 @@ class SlackInternalAlert:
 
     def post(self, message, message_type="notice"):
 
-        message_type_prefix = {
-            "error": "🚨",
-            "success": "👍",
-            "notice" : "ℹ️"
-        }
-        formatted_message = f"{message_type_prefix[message_type]} {self.script_name}: {message}" 
+        message_type_prefix = {"error": "🚨", "success": "👍", "notice": "ℹ️"}
+        formatted_message = (
+            f"{message_type_prefix[message_type]} {self.script_name}: {message}"
+        )
         try:
-            self.client.chat_postMessage(channel=self.slack_alert_channel_id, text=formatted_message)
+            self.client.chat_postMessage(
+                channel=self.slack_alert_channel_id, text=formatted_message
+            )
         except SlackApiError as e:
-            logger.error(f"Slack error: {e.response['error']} (channel: {self.slack_alert_channel_id})")
+            logger.error(
+                f"Slack error: {e.response['error']} (channel: {self.slack_alert_channel_id})"
+            )
